63c1886098e914f18733ba44057f2e172cd8fc17
gemtunde <gemtunde@gmail.com>
2025-04-07 03:10:42 +0100

    sidebar and header layut

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,22 @@
+"use client";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import Asidebar from "./_components/Asidebar";
+import Header from "./_components/Header";
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div
-    //  className="flex flex-col items-center justify-center min-h-screen py-6 bg-background"
-    >
-      {children}
-    </div>
+    <SidebarProvider>
+      <Asidebar />
+      <SidebarInset>
+        <main className="w-full">
+          <Header />
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
